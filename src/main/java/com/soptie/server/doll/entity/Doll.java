4b3d262d801c8c551313e440d63e7b8791d5069e
thguss <thgus345@naver.com>
2024-01-06 01:04:32 +0900

    [ADD] 인형 타입 추가

--- src/main/java/com/soptie/server/doll/entity/Doll.java
+++ src/main/java/com/soptie/server/doll/entity/Doll.java
@@ -2,6 +2,8 @@ package com.soptie.server.doll.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,4 +19,7 @@ public class Doll {
 	private Long id;
 
 	private String imageUrl;
+
+	@Enumerated(value = EnumType.STRING)
+	private DollType type;
 }
---
+++ src/main/java/com/soptie/server/doll/entity/DollType.java
@@ -0,0 +1,4 @@
+package com.soptie.server.doll.entity;
+
+public enum DollType {
+}
