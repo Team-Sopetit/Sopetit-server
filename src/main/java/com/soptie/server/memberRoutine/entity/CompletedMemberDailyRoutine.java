246d3fd5fb79671a9f0de3dacaf77954d4aad2d2
thguss <thgus345@naver.com>
2024-01-06 01:03:54 +0900

    [ADD] 삭제한 데일리루틴을 저장하는 엔티티 추가

---
+++ src/main/java/com/soptie/server/memberRoutine/entity/CompletedMemberDailyRoutine.java
@@ -0,0 +1,34 @@
+package com.soptie.server.memberRoutine.entity;
+
+import com.soptie.server.member.entity.Member;
+import com.soptie.server.routine.entity.daily.DailyRoutine;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class CompletedMemberDailyRoutine {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_routine_id")
+	private Long id;
+
+	private int achieveCount;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "routine_id")
+	private DailyRoutine routine;
+}
