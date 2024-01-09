7cae5feff017545da5fa4748b0e4cad12da8516a
choeseungbin <csb9427@naver.com>
2024-01-10 00:23:04 +0900

    [FEAT] deploy코드 추가 및 appspec.yml작성

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+REPOSITORY=/home/ubuntu/build
+cd $REPOSITORY
+
+APP_NAME=pearls
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  sudo kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포"
+nohup java -jar /home/ubuntu/build/build/libs/pearls-1.0.jar > /dev/null 2> /dev/null < /dev/null &
