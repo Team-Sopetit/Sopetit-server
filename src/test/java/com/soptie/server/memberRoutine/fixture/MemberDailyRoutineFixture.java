e6eec5856ae5914d4fa9673994a547f7f992fc09
thguss <thgus345@naver.com>
2024-01-10 01:34:41 +0900

    [TEST] 회원 데일리 루틴 추가 테스트

---
+++ src/test/java/com/soptie/server/memberRoutine/fixture/MemberDailyRoutineFixture.java
@@ -0,0 +1,10 @@
+package com.soptie.server.memberRoutine.fixture;
+
+import com.soptie.server.memberRoutine.dto.MemberDailyRoutineResponse;
+
+public class MemberDailyRoutineFixture {
+
+	public static MemberDailyRoutineResponse createMemberDailyRoutineResponseDTO() {
+		return new MemberDailyRoutineResponse(1L);
+	}
+}
