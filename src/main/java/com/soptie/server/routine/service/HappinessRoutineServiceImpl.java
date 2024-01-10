f4333f75ae8835a09694e6ca0b28a2e71e31e4c9
choeseungbin <csb9427@naver.com>
2024-01-10 23:52:01 +0900

    [FEAT] 행복루틴 테마 리스트 조회

---
+++ src/main/java/com/soptie/server/routine/service/HappinessRoutineServiceImpl.java
@@ -0,0 +1,21 @@
+package com.soptie.server.routine.service;
+
+import com.soptie.server.routine.dto.HappinessThemesResponse;
+import com.soptie.server.routine.repository.happiness.theme.HappinessThemeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class HappinessRoutineServiceImpl implements HappinessRoutineService{
+
+    private final HappinessThemeRepository happinessThemeRepository;
+    @Override
+    public HappinessThemesResponse getHappinessThemes() {
+        val themes = happinessThemeRepository.findAllOrderByNameAsc();
+        return HappinessThemesResponse.of(themes);
+    }
+}
