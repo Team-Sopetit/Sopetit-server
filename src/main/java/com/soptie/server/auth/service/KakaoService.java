30850a4603bf7ed03ba1d9e0ad5271498caea3be
Chan531 <chan657312@gmail.com>
2024-01-10 22:46:02 +0900

    [REFACTOR] 생성자 주입 방식 변경

--- src/main/java/com/soptie/server/auth/service/KakaoService.java
+++ src/main/java/com/soptie/server/auth/service/KakaoService.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,14 +17,14 @@ import java.util.Map;
 public class KakaoService {
 
     private final ValueConfig valueConfig;
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
     protected String getKakaoData(String socialAccessToken) {
-        val restTemplate = new RestTemplate();
         val headers = new HttpHeaders();
         headers.add("Authorization", socialAccessToken);
         val httpEntity = new HttpEntity<JsonArray>(headers);
         val responseData = restTemplate.postForEntity(valueConfig.getUserInfoUri(), httpEntity, Object.class);
-        val objectMapper = new ObjectMapper();
         return objectMapper.convertValue(responseData.getBody(), Map.class).get("id").toString();
     }
 }
