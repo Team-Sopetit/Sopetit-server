8027337662638846686dd67e1ee9ea922ca8d3fb
Chan531 <chan657312@gmail.com>
2024-01-12 21:48:27 +0900

    [TEST] 솜뭉치 주기 api test

--- src/test/java/com/soptie/server/member/controller/MemberControllerTest.java
+++ src/test/java/com/soptie/server/member/controller/MemberControllerTest.java
@@ -3,7 +3,11 @@ package com.soptie.server.member.controller;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.soptie.server.base.BaseControllerTest;
 import com.soptie.server.common.dto.Response;
+import com.soptie.server.member.dto.CottonCountResponse;
 import com.soptie.server.member.dto.MemberProfileRequest;
+import com.soptie.server.member.entity.CottonType;
+import com.soptie.server.routine.dto.DailyRoutinesResponse;
+import com.soptie.server.routine.fixture.DailyRoutineFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,6 +15,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.net.URI;
 import java.security.Principal;
@@ -19,7 +26,10 @@ import java.util.List;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static com.soptie.server.common.dto.Response.success;
 import static com.soptie.server.doll.entity.DollType.BROWN;
+import static com.soptie.server.member.entity.CottonType.DAILY;
 import static com.soptie.server.member.message.ResponseMessage.SUCCESS_CREATE_PROFILE;
+import static com.soptie.server.member.message.ResponseMessage.SUCCESS_GIVE_COTTON;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
@@ -28,6 +38,7 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MemberController.class)
@@ -82,5 +93,45 @@ class MemberControllerTest extends BaseControllerTest {
                                                 )
                                                 .build())))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("솜뭉치 주기")
+    void success_giveCotton() throws Exception {
+        // given
+        CottonCountResponse response = CottonCountResponse.of(0);
+        ResponseEntity<Response> result = ResponseEntity.ok(Response.success(SUCCESS_GIVE_COTTON.getMessage(), response));
+
+        // when
+        when(controller.giveCotton(principal, DAILY)).thenReturn(result);
+
+        // then
+        mockMvc
+                .perform(
+                        RestDocumentationRequestBuilders.patch(DEFAULT_URL + "/{cottonType}", DAILY)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .principal(principal))
+                .andDo(
+                        MockMvcRestDocumentation.document(
+                                "give-cotton-docs",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(
+                                        ResourceSnippetParameters.builder()
+                                                .tag(TAG)
+                                                .description("솜뭉치 주기")
+                                                .pathParameters(
+                                                        parameterWithName("cottonType").description("솜뭉치 종류")
+                                                )
+                                                .requestFields()
+                                                .responseFields(
+                                                        fieldWithPath("success").type(BOOLEAN).description("응답 성공 여부"),
+                                                        fieldWithPath("message").type(STRING).description("응답 메시지"),
+                                                        fieldWithPath("data").type(OBJECT).description("응답 데이터"),
+                                                        fieldWithPath("data.cottonCount").type(NUMBER).description("남은 솜뭉치 개수")
+                                                )
+                                                .build())))
+                .andExpect(status().isOk());
     }
 }
