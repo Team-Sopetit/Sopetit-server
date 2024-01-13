8c095b762a01dc2c31f967b125ef5acb7249611f
choeseungbin <csb9427@naver.com>
2024-01-13 14:21:05 +0900

    [FIX] lombok 선언

--- src/main/java/com/soptie/server/routine/entity/happiness/RoutineImage.java
+++ src/main/java/com/soptie/server/routine/entity/happiness/RoutineImage.java
@@ -3,8 +3,10 @@ package com.soptie.server.routine.entity.happiness;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class RoutineImage {
