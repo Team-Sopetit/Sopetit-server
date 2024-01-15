af1e18b7b080a9adde35a55b373ca103167bee3e
Chan531 <chan657312@gmail.com>
2024-01-16 00:55:13 +0900

    [CHORE] 리뷰 반영 수정

--- src/main/java/com/soptie/server/routine/dto/HappinessSubRoutinesResponse.java
+++ src/main/java/com/soptie/server/routine/dto/HappinessSubRoutinesResponse.java
@@ -28,7 +28,6 @@ public record HappinessSubRoutinesResponse(
 
     @Builder
     public record HappinessSubRoutineResponse(
-
             Long subRoutineId,
             String content,
             String detailContent,
