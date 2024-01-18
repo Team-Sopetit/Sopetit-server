95716c26ce2de3a1f6de22ca62c4de9baf9b1380
thguss <thgus345@naver.com>
2024-01-18 17:35:29 +0900

    [ADD] 테마별 데일리 루틴 조회, 배경 이미지 추가

--- src/test/java/com/soptie/server/routine/fixture/DailyRoutineFixture.java
+++ src/test/java/com/soptie/server/routine/fixture/DailyRoutineFixture.java
@@ -3,7 +3,9 @@ package com.soptie.server.routine.fixture;
 import java.util.List;
 import java.util.stream.Stream;
 
-import com.soptie.server.routine.dto.DailyRoutinesResponse;
+import com.soptie.server.routine.dto.DailyRoutineResponse;
+import com.soptie.server.routine.dto.DailyRoutinesByThemeResponse;
+import com.soptie.server.routine.dto.DailyRoutinesByThemesResponse;
 import com.soptie.server.routine.dto.DailyThemesResponse;
 
 public class DailyRoutineFixture {
@@ -19,26 +21,27 @@ public class DailyRoutineFixture {
 
 	private static DailyThemesResponse.DailyThemeResponse createDailyThemeResponse(int i) {
 		return DailyThemesResponse.DailyThemeResponse.builder()
-				.themeId((long)i)
+				.themeId(i)
 				.name("theme name" + i)
 				.iconImageUrl("https://...")
 				.backgroundImageUrl("https://...")
 				.build();
 	}
 
-	public static DailyRoutinesResponse createDailyRoutinesResponseDTO() {
-		return new DailyRoutinesResponse(createDailyRoutineResponses());
+	public static DailyRoutinesByThemesResponse createDailyRoutinesByThemesResponseDTO() {
+		return new DailyRoutinesByThemesResponse(createDailyRoutineResponses());
 	}
 
-	private static List<DailyRoutinesResponse.DailyRoutineResponse> createDailyRoutineResponses() {
+	public static DailyRoutinesByThemeResponse createDailyRoutinesByThemeResponseDTO() {
+		return new DailyRoutinesByThemeResponse("https://...", createDailyRoutineResponses());
+	}
+
+	private static List<DailyRoutineResponse> createDailyRoutineResponses() {
 		return Stream.iterate(1, i -> i + 1).limit(5)
 				.map(DailyRoutineFixture::createDailyRoutineResponse).toList();
 	}
 
-	private static DailyRoutinesResponse.DailyRoutineResponse createDailyRoutineResponse(int i) {
-		return DailyRoutinesResponse.DailyRoutineResponse.builder()
-				.routineId((long)i)
-				.content("routine content" + i)
-				.build();
+	private static DailyRoutineResponse createDailyRoutineResponse(int i) {
+		return new DailyRoutineResponse((long)i, "routine content" + i);
 	}
 }
