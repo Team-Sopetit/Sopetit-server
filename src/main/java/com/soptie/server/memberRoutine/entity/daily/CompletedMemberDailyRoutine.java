5a8c94ee190bbe61695271d7cf51733e4c73b45b
thguss <thgus345@naver.com>
2024-01-19 21:24:38 +0900

    [ADD] createdAt 자동화 추가

--- src/main/java/com/soptie/server/memberRoutine/entity/daily/CompletedMemberDailyRoutine.java
+++ src/main/java/com/soptie/server/memberRoutine/entity/daily/CompletedMemberDailyRoutine.java
@@ -3,12 +3,14 @@ package com.soptie.server.memberRoutine.entity.daily;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.soptie.server.member.entity.Member;
 import com.soptie.server.routine.entity.daily.DailyRoutine;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +23,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class CompletedMemberDailyRoutine {
 
 	@Id
@@ -41,7 +44,6 @@ public class CompletedMemberDailyRoutine {
 	private DailyRoutine routine;
 
 	@CreatedDate
-	@Column(nullable = false)
 	protected LocalDateTime createdAt;
 
 	public CompletedMemberDailyRoutine(MemberDailyRoutine routine) {
