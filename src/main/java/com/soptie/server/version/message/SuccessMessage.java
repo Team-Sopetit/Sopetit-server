98aa7bd375695da6d9eebe5e2e4b1789a1ac4b95
thguss <thgus345@naver.com>
2024-01-29 12:42:10 +0900

    [FEAT] 앱 버전 조회

---
+++ src/main/java/com/soptie/server/version/message/SuccessMessage.java
@@ -0,0 +1,14 @@
+package com.soptie.server.version.message;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum SuccessMessage {
+
+	SUCCESS_GET_APP_VERSION("버전 정보 조회 성공"),
+	;
+
+	private final String message;
+}
