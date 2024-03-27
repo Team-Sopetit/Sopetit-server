265c13472585e4ed00d38e03c630df2c7f5d2a17
Chan531 <chan657312@gmail.com>
2024-03-27 13:28:31 +0900

    [TEST] DollService 테스트

--- src/main/java/com/soptie/server/doll/entity/DollImage.java
+++ src/main/java/com/soptie/server/doll/entity/DollImage.java
@@ -1,11 +1,14 @@
 package com.soptie.server.doll.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 @Getter
 public class DollImage {
 
