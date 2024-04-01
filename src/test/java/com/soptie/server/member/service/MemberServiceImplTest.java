784dec2f57fb126d8ec9c7dce72e8e98615b899f
Chan531 <chan657312@gmail.com>
2024-04-01 14:16:46 +0900

    [TEST] createMemberProfile() 테스트

--- src/test/java/com/soptie/server/member/service/MemberServiceImplTest.java
+++ src/test/java/com/soptie/server/member/service/MemberServiceImplTest.java
@@ -1,11 +1,14 @@
 package com.soptie.server.member.service;
 
-import com.soptie.server.member.entity.Cotton;
+import com.soptie.server.doll.entity.DollType;
+import com.soptie.server.member.dto.MemberProfileRequest;
 import com.soptie.server.member.entity.CottonType;
 import com.soptie.server.member.entity.Member;
 import com.soptie.server.member.exception.MemberException;
 import com.soptie.server.member.repository.MemberRepository;
 import com.soptie.server.memberDoll.entity.MemberDoll;
+import com.soptie.server.memberDoll.service.MemberDollServiceImpl;
+import com.soptie.server.memberRoutine.service.MemberDailyRoutineServiceImpl;
 import com.soptie.server.support.MemberFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,12 +17,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
+import static com.soptie.server.doll.entity.DollType.BROWN;
 import static com.soptie.server.member.message.ErrorCode.NOT_ENOUGH_COTTON;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceImplTest {
@@ -28,7 +33,34 @@ class MemberServiceImplTest {
     private MemberServiceImpl memberService;
 
     @Mock
+    private MemberDailyRoutineServiceImpl memberDailyRoutineService;
+
+    @Mock
+    private MemberDollServiceImpl memberDollService;
+
+    @Mock
     private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("멤버 프로필 생성 시, 멤버 데일리 루틴 생성과 멤버 인형 생성 메소드를 호출한다.")
+    void 멤버_프로필을_생성하면서_멤버_데일리_루틴과_멤버_인형을_생성한다() {
+        // given
+        long memberId = 1L;
+        Member member = member(memberId);
+        DollType dollType = BROWN;
+        String name = "memberDoll";
+        List<Long> routines = List.of(2L, 3L, 4L);
+        MemberProfileRequest request = new MemberProfileRequest(dollType, name, routines);
+        doNothing().when(memberDailyRoutineService).createMemberDailyRoutines(member, List.of(2L, 3L, 4L));
+        doNothing().when(memberDollService).createMemberDoll(member, dollType, name);
+
+        // when
+        memberService.createMemberProfile(memberId, request);
+
+        // then
+        verify(memberDailyRoutineService).createMemberDailyRoutines(member, routines);
+        verify(memberDollService).createMemberDoll(member, dollType, name);
+    }
 
     @Test
     @DisplayName("솜뭉치 개수가 양수일 때 솜뭉치를 줄 수 있다.")
@@ -58,6 +90,12 @@ class MemberServiceImplTest {
         assertThatThrownBy(() -> memberService.giveCotton(member.getId(), CottonType.DAILY))
                 .isInstanceOf(MemberException.class)
                 .hasMessage("[MemberException] : " + NOT_ENOUGH_COTTON.getMessage());
+    }
+
+    private Member member(long memberId) {
+        Member member = MemberFixture.member().id(memberId).build();
+        doReturn(Optional.of(member)).when(memberRepository).findById(1L);
+        return member;
     }
 
     private Member member(long memberId, MemberDoll memberDoll) {
