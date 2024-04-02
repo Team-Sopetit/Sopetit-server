ae9ef4d41dbb83cdc0e3d571252b761c2e537d66
thguss <thgus345@naver.com>
2024-04-02 15:47:06 +0900

    [DELETE] 읽기 전용 Swagger 관련 파일 삭제

--- src/test/java/com/soptie/server/base/BaseServiceTest.java
+++
@@ -1,8 +0,0 @@
-package com.soptie.server.base;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public abstract class BaseServiceTest {
-}
