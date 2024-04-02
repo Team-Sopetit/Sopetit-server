1f118bc19da0cae0182c9e5992fb266c4ed19d5a
thguss <thgus345@naver.com>
2024-04-02 18:35:33 +0900

    [ADD] 스웨거 연결

--- src/main/java/com/soptie/server/doll/controller/DollController.java
+++ src/main/java/com/soptie/server/doll/controller/DollController.java
@@ -1,6 +1,6 @@
 package com.soptie.server.doll.controller;
 
-import static com.soptie.server.common.dto.Response.*;
+import static com.soptie.server.common.dto.SuccessResponse.*;
 import static com.soptie.server.doll.message.SuccessMessage.*;
 
 import org.springframework.http.ResponseEntity;
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.soptie.server.common.dto.Response;
+import com.soptie.server.common.dto.SuccessResponse;
+import com.soptie.server.doll.dto.DollImageResponse;
 import com.soptie.server.doll.entity.DollType;
 import com.soptie.server.doll.service.DollService;
 
@@ -18,13 +19,13 @@ import lombok.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/dolls")
-public class DollController {
+public class DollController implements DollApi {
 
 	private final DollService dollService;
 
 	@GetMapping("/image/{type}")
-	public ResponseEntity<Response> getDollImages(@PathVariable DollType type) {
+	public ResponseEntity<SuccessResponse<DollImageResponse>> getDollImages(@PathVariable DollType type) {
 		val response = dollService.getDollImage(type);
-		return ResponseEntity.ok(success(SUCCESS_GET_IMAGE.getMessage(), response));
+		return ResponseEntity.ok(of(SUCCESS_GET_IMAGE.getMessage(), response));
 	}
 }
