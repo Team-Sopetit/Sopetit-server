0d1480cda14ec4118165eedfaa31fd4d2123c6db
thguss <thgus345@naver.com>
2024-04-02 18:11:51 +0900

    [ADD] 스웨거 어노테이션 추가

--- src/main/java/com/soptie/server/test/TestController.java
+++ src/main/java/com/soptie/server/test/TestController.java
@@ -1,16 +1,20 @@
 package com.soptie.server.test;
 
+import static com.soptie.server.common.dto.SuccessResponse.*;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.soptie.server.common.dto.BaseResponse;
+
 @RestController
 @RequestMapping("/api/v1/test")
-public class TestController {
+public class TestController implements TestApi {
 
 	@GetMapping
-	public ResponseEntity<String> test() {
-		return ResponseEntity.ok("Success to server connect.");
+	public ResponseEntity<BaseResponse> test() {
+		return ResponseEntity.ok(success("Success to server connect."));
 	}
 }
