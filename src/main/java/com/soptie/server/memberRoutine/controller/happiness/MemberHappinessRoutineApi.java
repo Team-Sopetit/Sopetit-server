acd6540d90fd61943b5f4b24e752e3c0da1d2cda
choeseungbin <csb9427@naver.com>
2024-04-08 22:50:41 +0900

    [ADD] 스웨거 정보 추가

--- src/main/java/com/soptie/server/memberRoutine/controller/happiness/MemberHappinessRoutineApi.java
+++ src/main/java/com/soptie/server/memberRoutine/controller/happiness/MemberHappinessRoutineApi.java
@@ -1,32 +1,40 @@
 package com.soptie.server.memberRoutine.controller.happiness;
 
-import java.security.Principal;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import com.soptie.server.common.dto.BaseResponse;
 import com.soptie.server.common.dto.ErrorResponse;
 import com.soptie.server.common.dto.SuccessResponse;
 import com.soptie.server.memberRoutine.dto.MemberHappinessRoutineRequest;
 import com.soptie.server.memberRoutine.dto.MemberHappinessRoutineResponse;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.security.Principal;
 
 @Tag(name = "member happiness routines", description = "회원의 행복 루틴 API")
 public interface MemberHappinessRoutineApi {
 
     @Operation(
-            summary = "",
-            description = "",
+            summary = "행복 루틴 추가",
+            description = "회원의 행복 루틴을 추가한다.",
             responses = {
                     @ApiResponse(responseCode = "200", description = "성공"),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "유효하지 않은 토큰",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "4xx",
+                            description = "클라이언트(요청) 오류",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    ),
                     @ApiResponse(
                             responseCode = "500",
                             description = "서버 내부 오류",
@@ -40,8 +48,8 @@ public interface MemberHappinessRoutineApi {
     );
 
     @Operation(
-            summary = "",
-            description = "",
+            summary = "회원의 행복 루틴 목록 조회",
+            description = "회원의 행복 루틴을 조회한다.",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
@@ -53,6 +61,16 @@ public interface MemberHappinessRoutineApi {
                             description = "데이터 없음",
                             content = @Content(schema = @Schema(implementation = BaseResponse.class))),
                     @ApiResponse(
+                            responseCode = "401",
+                            description = "유효하지 않은 토큰",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "4xx",
+                            description = "클라이언트(요청) 오류",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    ),
+                    @ApiResponse(
                             responseCode = "500",
                             description = "서버 내부 오류",
                             content = @Content(schema = @Schema(implementation = ErrorResponse.class))
@@ -62,13 +80,23 @@ public interface MemberHappinessRoutineApi {
     ResponseEntity<?> getMemberHappinessRoutine(@Parameter(hidden = true) Principal principal);
 
     @Operation(
-            summary = "",
-            description = "",
+            summary = "행복 루틴 삭제",
+            description = "회원의 행복 루틴을 삭제한다.",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
                             description = "성공",
                             content = @Content(schema = @Schema(implementation = SuccessResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "유효하지 않은 토큰",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "4xx",
+                            description = "클라이언트(요청) 오류",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
                     ),
                     @ApiResponse(
                             responseCode = "500",
@@ -83,13 +111,23 @@ public interface MemberHappinessRoutineApi {
     );
 
     @Operation(
-            summary = "",
-            description = "",
+            summary = "행복 루틴 달성",
+            description = "회원의 행복 루틴을 달성한다.",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
                             description = "성공",
                             content = @Content(schema = @Schema(implementation = SuccessResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "유효하지 않은 토큰",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "4xx",
+                            description = "클라이언트(요청) 오류",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
                     ),
                     @ApiResponse(
                             responseCode = "500",
