a944546443ff79bdb8299f989ce1ef8d617f2220
thguss <thgus345@naver.com>
2024-04-14 01:17:13 +0900

    [CHORE] 통합 테스트 환경 세팅

---
+++ src/test/java/com/soptie/server/support/IntegrationTest.java
@@ -0,0 +1,19 @@
+package com.soptie.server.support;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+
+@SpringBootTest
+@TestExecutionListeners(
+		value = {DatabaseClearListener.class, RestMockListener.class},
+		mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
+)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface IntegrationTest {
+}
