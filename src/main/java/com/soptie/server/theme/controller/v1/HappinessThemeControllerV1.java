592d44281dd223de840de3d7e8c9029897376cd7
thguss <thgus345@naver.com>
2024-04-30 13:18:55 +0900

    [ADD] 행복 루틴 테마 목록 조회 추가 (이동)

---
+++ src/main/java/com/soptie/server/theme/controller/v1/HappinessThemeControllerV1.java
@@ -0,0 +1,30 @@
+package com.soptie.server.theme.controller.v1;
+
+import static com.soptie.server.common.dto.SuccessResponse.*;
+import static com.soptie.server.routine.message.SuccessMessage.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soptie.server.common.dto.SuccessResponse;
+import com.soptie.server.theme.controller.v1.dto.response.HappinessThemeListGetResponse;
+import com.soptie.server.theme.service.ThemeService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/routines/happiness/themes")
+public class HappinessThemeControllerV1 implements HappinessThemeApiV1 {
+
+    private final ThemeService themeService;
+
+    @GetMapping
+    public ResponseEntity<SuccessResponse<HappinessThemeListGetResponse>> getHappinessThemes() {
+        val response = HappinessThemeListGetResponse.of(themeService.getThemes());
+        return ResponseEntity.ok(of(SUCCESS_GET_HAPPINESS_THEME.getMessage(), response));
+    }
+}
