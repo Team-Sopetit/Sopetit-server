d24755f8cb083f62a59f9b9b4c6e79fb84b5aaa0
thguss <thgus345@naver.com>
2024-04-30 13:11:59 +0900

    [DELETE] 행복 루틴 테마 전체 조회 기능 삭제 (이동)

--- src/main/java/com/soptie/server/routine/service/HappinessRoutineService.java
+++ src/main/java/com/soptie/server/routine/service/HappinessRoutineService.java
@@ -2,10 +2,8 @@ package com.soptie.server.routine.service;
 
 import com.soptie.server.routine.dto.HappinessRoutinesResponse;
 import com.soptie.server.routine.dto.HappinessSubRoutinesResponse;
-import com.soptie.server.routine.dto.HappinessThemesResponse;
 
 public interface HappinessRoutineService {
-    HappinessThemesResponse getHappinessThemes();
     HappinessRoutinesResponse getHappinessRoutinesByTheme(Long themeId);
     HappinessSubRoutinesResponse getHappinessSubRoutines(long routineId);
 }
