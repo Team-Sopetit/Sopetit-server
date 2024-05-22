6863a8abea9845340203acba4cb03df551769587
Chan531 <chan657312@gmail.com>
2024-05-22 21:20:54 +0900

    [ADD] MemberDeleter 추가

---
+++ src/main/java/com/soptie/server/member/adapter/MemberDeleter.java
@@ -0,0 +1,17 @@
+package com.soptie.server.member.adapter;
+
+import com.soptie.server.common.support.RepositoryAdapter;
+import com.soptie.server.member.entity.Member;
+import com.soptie.server.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@RepositoryAdapter
+@RequiredArgsConstructor
+public class MemberDeleter {
+
+    private final MemberRepository memberRepository;
+
+    public void deleteMember(Member member) {
+        memberRepository.delete(member);
+    }
+}
