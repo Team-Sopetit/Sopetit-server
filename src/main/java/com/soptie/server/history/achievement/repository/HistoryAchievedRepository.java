ed5ec6aeec16a9b9aac7275624e565051eaa0891
thguss <thgus345@naver.com>
2024-06-18 00:26:41 +0900

    [ADD] 회원의 루틴 이력 데이터 삭제

--- src/main/java/com/soptie/server/history/achievement/repository/HistoryAchievedRepository.java
+++ src/main/java/com/soptie/server/history/achievement/repository/HistoryAchievedRepository.java
@@ -3,6 +3,8 @@ package com.soptie.server.history.achievement.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.soptie.server.history.achievement.entity.HistoryAchieved;
+import com.soptie.server.member.entity.Member;
 
 public interface HistoryAchievedRepository extends JpaRepository<HistoryAchieved, Long>, HistoryAchievedCustomRepository {
+	void deleteByMember(Member member);
 }
