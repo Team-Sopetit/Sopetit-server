a6ccc39691aaa1b0f65c899ac5f28b26f0ffe4cb
thguss <thgus345@naver.com>
2024-06-22 14:08:39 +0900

    [CHORE] 한 줄 띄어쓰기 리팩토링

--- src/main/java/com/soptie/server/common/dto/SuccessResponse.java
+++ src/main/java/com/soptie/server/common/dto/SuccessResponse.java
@@ -9,8 +9,11 @@ import lombok.Builder;
 import lombok.NonNull;
 
 @Builder(access = PRIVATE)
-public record SuccessResponse<T>(boolean success, @NonNull String message, @JsonInclude(value = NON_NULL) T data)
-	implements BaseResponse {
+public record SuccessResponse<T>(
+	boolean success,
+	@NonNull String message,
+	@JsonInclude(value = NON_NULL) T data
+) implements BaseResponse {
 
 	public static <T> SuccessResponse<T> success(String message, T data) {
 		return SuccessResponse.<T>builder().success(true).message(message).data(data).build();
