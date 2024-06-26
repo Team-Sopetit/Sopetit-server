f81426fbdbebc34f208015961ef10d4d8907ad90
thguss <thgus345@naver.com>
2024-06-27 01:36:30 +0900

    [FEAT] 테마별 데일리 루틴 조회(public) 기능 구현

--- src/main/java/com/soptie/server/routine/service/RoutineService.java
+++ src/main/java/com/soptie/server/routine/service/RoutineService.java
@@ -1,5 +1,12 @@
 package com.soptie.server.routine.service;
 
+import static com.soptie.server.routine.entity.RoutineType.*;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +20,7 @@ import com.soptie.server.routine.service.dto.request.HappinessSubRoutineListGetS
 import com.soptie.server.routine.service.dto.response.DailyRoutineListGetServiceResponse;
 import com.soptie.server.routine.service.dto.response.HappinessRoutineListGetServiceResponse;
 import com.soptie.server.routine.service.dto.response.HappinessSubRoutineListGetServiceResponse;
+import com.soptie.server.routine.service.vo.RoutineVO;
 import com.soptie.server.theme.adapter.ThemeFinder;
 
 import lombok.RequiredArgsConstructor;
@@ -53,5 +61,15 @@ public class RoutineService {
 		val routine = routineFinder.findById(request.routineId());
 		val subRoutines = challengeFinder.findByRoutine(routine);
 		return HappinessSubRoutineListGetServiceResponse.of(routine, subRoutines);
+	}
+
+	public Map<Long, List<RoutineVO>> acquireAllInDailyWithThemeId(Set<Long> themeIds) {
+		System.out.println(themeIds);
+		val themeToRoutine = new LinkedHashMap<Long, List<RoutineVO>>();
+		for (val themeId : themeIds) {
+			val routines = routineFinder.findAllByTypeAndThemeId(DAILY, themeId);
+			themeToRoutine.put(themeId, routines);
+		}
+		return themeToRoutine;
 	}
 }
