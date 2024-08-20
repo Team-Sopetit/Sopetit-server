332d095e85dec89e4a88ae786191ef8c5bbd7117
thguss <thgus345@naver.com>
2024-08-20 23:41:49 +0900

    [DELETE] deleted v1

--- src/main/java/com/soptie/server/domain/usecase/DollService.java
+++
@@ -1,8 +0,0 @@
-package com.soptie.server.domain.usecase;
-
-import com.soptie.server.api.controller.dto.response.doll.DollImageResponse;
-import com.soptie.server.domain.doll.DollType;
-
-public interface DollService {
-	DollImageResponse getDollImage(DollType type);
-}
