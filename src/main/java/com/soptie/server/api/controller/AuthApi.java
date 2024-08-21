da7be66733d839e4913ab8e405630d414659422f
Chan531 <chan657312@gmail.com>
2024-08-21 14:25:58 +0900

    [REFACTOR] refactor auth api

--- src/main/java/com/soptie/server/api/controller/AuthApi.java
+++ src/main/java/com/soptie/server/api/controller/AuthApi.java
@@ -1,27 +1,24 @@
 package com.soptie.server.api.controller;
 
-import static com.soptie.server.common.message.AuthMessage.*;
-import static com.soptie.server.api.controller.dto.response.SuccessResponse.*;
-
 import java.security.Principal;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.soptie.server.api.controller.docs.AuthApiDocs;
-import com.soptie.server.domain.usecase.AuthService;
-import com.soptie.server.domain.auth.SignInServiceRequest;
-import com.soptie.server.domain.auth.TokenGetServiceRequest;
+import com.soptie.server.api.controller.dto.request.auth.SignInRequest;
 import com.soptie.server.api.controller.dto.response.BaseResponse;
 import com.soptie.server.api.controller.dto.response.SuccessResponse;
-import com.soptie.server.api.controller.dto.request.auth.SignInRequest;
 import com.soptie.server.api.controller.dto.response.auth.SignInResponse;
 import com.soptie.server.api.controller.dto.response.auth.TokenGetResponse;
+import com.soptie.server.api.controller.generic.SuccessMessage;
+import com.soptie.server.domain.auth.AuthService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.val;
@@ -34,33 +31,37 @@ public class AuthApi implements AuthApiDocs {
 	private final AuthService authService;
 
 	@PostMapping
-	public ResponseEntity<SuccessResponse<SignInResponse>> signIn(
+	@ResponseStatus(HttpStatus.OK)
+	public SuccessResponse<SignInResponse> signIn(
 		@RequestHeader("Authorization") String socialAccessToken,
 		@RequestBody SignInRequest request
 	) {
-		val response = SignInResponse.of(authService.signIn(SignInServiceRequest.of(socialAccessToken, request)));
-		return ResponseEntity.ok(success(SUCCESS_SIGN_IN.getMessage(), response));
+		val response = authService.signIn(socialAccessToken, request);
+		return SuccessResponse.success(SuccessMessage.SUCCESS_SIGN_IN.getMessage(), response);
 	}
 
 	@PostMapping("/token")
-	public ResponseEntity<SuccessResponse<TokenGetResponse>> reissueToken(
+	@ResponseStatus(HttpStatus.OK)
+	public SuccessResponse<TokenGetResponse> reissueToken(
 		@RequestHeader("Authorization") String refreshToken
 	) {
-		val response = TokenGetResponse.of(authService.reissueToken(TokenGetServiceRequest.of(refreshToken)));
-		return ResponseEntity.ok(success(SUCCESS_RECREATE_TOKEN.getMessage(), response));
+		val response = authService.reissueToken(refreshToken);
+		return SuccessResponse.success(SuccessMessage.SUCCESS_RECREATE_TOKEN.getMessage(), response);
 	}
 
 	@PostMapping("/logout")
-	public ResponseEntity<BaseResponse> signOut(Principal principal) {
+	@ResponseStatus(HttpStatus.OK)
+	public BaseResponse signOut(Principal principal) {
 		val memberId = Long.parseLong(principal.getName());
 		authService.signOut(memberId);
-		return ResponseEntity.ok(success(SUCCESS_SIGN_OUT.getMessage()));
+		return SuccessResponse.success(SuccessMessage.SUCCESS_SIGN_OUT.getMessage());
 	}
 
 	@DeleteMapping
-	public ResponseEntity<BaseResponse> withdrawal(Principal principal) {
+	@ResponseStatus(HttpStatus.OK)
+	public BaseResponse withdrawal(Principal principal) {
 		val memberId = Long.parseLong(principal.getName());
 		authService.withdraw(memberId);
-		return ResponseEntity.ok(success(SUCCESS_WITHDRAWAL.getMessage()));
+		return SuccessResponse.success(SuccessMessage.SUCCESS_WITHDRAWAL.getMessage());
 	}
 }
