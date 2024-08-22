8fcdb5aa1ed2bfa0574a37fafd789b6ba86cec2b
thguss <thgus345@naver.com>
2024-08-22 22:09:43 +0900

    [FIX] fixed invalid table name

--- src/main/java/com/soptie/server/persistence/repository/RoutineRepository.java
+++ src/main/java/com/soptie/server/persistence/repository/RoutineRepository.java
@@ -10,7 +10,7 @@ import com.soptie.server.persistence.entity.RoutineEntity;
 public interface RoutineRepository extends JpaRepository<RoutineEntity, Long> {
 	//TODO: 데이터베이스 내부 자체 처리 방법 찾아보기
 	@Query(
-		value = "SELECT * FROM routine_entity WHERE theme_id = :themeId ORDER BY content COLLATE \"ko_KR.utf8\" ASC",
+		value = "SELECT * FROM routine WHERE theme_id = :themeId ORDER BY content COLLATE \"ko_KR.utf8\" ASC",
 		nativeQuery = true)
 	List<RoutineEntity> findByThemeIdOrderByContentAsc(long themeId);
 
