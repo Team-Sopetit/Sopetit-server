1d970d5871b7bfe4aa0d69ee9d441f9a72d8e802
thguss <thgus345@naver.com>
2024-10-16 20:57:40 +0900

    fix: updated configuration by package

---
+++ src/main/java/com/soptie/server/batch/BatchProperties.java
@@ -0,0 +1,19 @@
+package com.soptie.server.batch;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "batch")
+public record BatchProperties(
+	Cron cron
+) {
+
+	public record Cron(
+		Init init
+	) {
+
+		public record Init(
+			String routine
+		) {
+		}
+	}
+}
--- src/main/java/com/soptie/server/config/JpaQueryFactoryConfig.java
+++ src/main/java/com/soptie/server/config/JpaQueryFactoryConfig.java
@@ -1,4 +1,4 @@
-package com.soptie.server.persistence.config;
+package com.soptie.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
