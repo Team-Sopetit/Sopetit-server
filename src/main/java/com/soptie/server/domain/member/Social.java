20dfff748c6dab1b85cb068b95cc36fdc2fe6f66
thguss <thgus345@naver.com>
2024-10-16 21:04:31 +0900

    add: added getter

--- src/main/java/com/soptie/server/domain/member/Social.java
+++ src/main/java/com/soptie/server/domain/member/Social.java
@@ -2,8 +2,10 @@ package com.soptie.server.domain.member;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class Social {
 	@NotNull
 	protected SocialType socialType;
