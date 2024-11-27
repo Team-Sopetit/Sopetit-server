80e044a379926647e2ab8704de1526b683882e70
Chan531 <chan657312@gmail.com>
2024-11-27 18:02:07 +0900

    feature: save memo

--- src/main/java/com/soptie/server/persistence/adapter/MemoAdapter.java
+++ src/main/java/com/soptie/server/persistence/adapter/MemoAdapter.java
@@ -1,6 +1,11 @@
 package com.soptie.server.persistence.adapter;
 
+import java.time.LocalDate;
+
 import com.soptie.server.common.support.RepositoryAdapter;
+import com.soptie.server.domain.member.Member;
+import com.soptie.server.domain.memo.Memo;
+import com.soptie.server.persistence.entity.MemoEntity;
 import com.soptie.server.persistence.repository.MemoRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,4 +15,8 @@ import lombok.RequiredArgsConstructor;
 public class MemoAdapter {
 
 	private final MemoRepository memoRepository;
+
+	public Memo save(final LocalDate achievedDate, final String content, final Member member) {
+		return memoRepository.save(new MemoEntity(achievedDate, content, member)).toDomain();
+	}
 }
