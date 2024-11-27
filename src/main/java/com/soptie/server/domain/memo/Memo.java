1a6a40a46269443c271a4bc054bdef83a265c8d1
Chan531 <chan657312@gmail.com>
2024-11-27 17:27:35 +0900

    feature: init memo

---
+++ src/main/java/com/soptie/server/domain/memo/Memo.java
@@ -0,0 +1,26 @@
+package com.soptie.server.domain.memo;
+
+import java.time.LocalDate;
+
+import com.soptie.server.domain.member.Member;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Memo {
+	private Long id;
+	private LocalDate achievedDate;
+	private String content;
+	private long memberId;
+
+	public Memo(LocalDate achievedDate, String content, Member member) {
+		this.achievedDate = achievedDate;
+		this.content = content;
+		this.memberId = member.getId();
+	}
+}
