96ef175252565733a598e0fbef3cde5b4caaf572
Chan531 <chan657312@gmail.com>
2024-11-27 17:27:53 +0900

    feature: init memo service

---
+++ src/main/java/com/soptie/server/domain/memo/MemoService.java
@@ -0,0 +1,12 @@
+package com.soptie.server.domain.memo;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemoService {
+}
