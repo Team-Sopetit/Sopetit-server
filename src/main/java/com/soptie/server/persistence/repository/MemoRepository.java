cd2e07fa1387062bd1675f28df4fca2bdda95362
Chan531 <chan657312@gmail.com>
2024-11-27 18:34:10 +0900

    feature: find memo
     find by id and member id

--- src/main/java/com/soptie/server/persistence/repository/MemoRepository.java
+++ src/main/java/com/soptie/server/persistence/repository/MemoRepository.java
@@ -1,8 +1,11 @@
 package com.soptie.server.persistence.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.soptie.server.persistence.entity.MemoEntity;
 
 public interface MemoRepository extends JpaRepository<MemoEntity, Long> {
+	Optional<MemoEntity> findByIdAndMemberId(long id, long memberId);
 }
