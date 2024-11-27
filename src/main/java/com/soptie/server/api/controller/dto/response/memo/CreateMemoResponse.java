6241bcb151e67a8c843553fbbce3906e560b6917
Chan531 <chan657312@gmail.com>
2024-11-27 18:02:36 +0900

    feature: memo create response

---
+++ src/main/java/com/soptie/server/api/controller/dto/response/memo/CreateMemoResponse.java
@@ -0,0 +1,20 @@
+package com.soptie.server.api.controller.dto.response.memo;
+
+import com.soptie.server.domain.memo.Memo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Builder;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record CreateMemoResponse(
+	@Schema(description = "추가한 메모 id", example = "1")
+	long id
+) {
+
+	public static CreateMemoResponse from(Memo memo) {
+		return CreateMemoResponse.builder()
+			.id(memo.getId())
+			.build();
+	}
+}
