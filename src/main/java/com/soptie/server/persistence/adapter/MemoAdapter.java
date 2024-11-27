af74384802f6f206eb7bb32a2973c58dc5d3e45b
Chan531 <chan657312@gmail.com>
2024-11-27 18:35:32 +0900

    feature: find memo

--- src/main/java/com/soptie/server/persistence/adapter/MemoAdapter.java
+++ src/main/java/com/soptie/server/persistence/adapter/MemoAdapter.java
@@ -2,6 +2,8 @@ package com.soptie.server.persistence.adapter;
 
 import java.time.LocalDate;
 
+import com.soptie.server.common.exception.ExceptionCode;
+import com.soptie.server.common.exception.SoftieException;
 import com.soptie.server.common.support.RepositoryAdapter;
 import com.soptie.server.domain.member.Member;
 import com.soptie.server.domain.memo.Memo;
@@ -18,5 +20,12 @@ public class MemoAdapter {
 
 	public Memo save(final LocalDate achievedDate, final String content, final Member member) {
 		return memoRepository.save(new MemoEntity(achievedDate, content, member)).toDomain();
+	}
+
+	public Memo findByIdAndMemberId(final long id, final long memberId) {
+		return memoRepository.findByIdAndMemberId(id, memberId)
+			.orElseThrow(() -> new SoftieException(
+				ExceptionCode.NOT_FOUND,
+				"Member ID: " + memberId + " Memo ID: " + id)).toDomain();
 	}
 }
