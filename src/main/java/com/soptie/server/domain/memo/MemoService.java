9f2660b2929afcdc0348663599892bbe67cd2079
Chan531 <chan657312@gmail.com>
2024-11-27 18:36:18 +0900

    feature: update memo

--- src/main/java/com/soptie/server/domain/memo/MemoService.java
+++ src/main/java/com/soptie/server/domain/memo/MemoService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.soptie.server.api.controller.dto.request.memo.CreateMemoRequest;
+import com.soptie.server.api.controller.dto.request.memo.ModifyMemoRequest;
 import com.soptie.server.api.controller.dto.response.memo.CreateMemoResponse;
 import com.soptie.server.persistence.adapter.MemberAdapter;
 import com.soptie.server.persistence.adapter.MemoAdapter;
@@ -24,5 +25,12 @@ public class MemoService {
 		val member = memberAdapter.findById(memberId);
 		val memo = memoAdapter.save(request.achievedDate(), request.content(), member);
 		return CreateMemoResponse.from(memo);
+	}
+
+	@Transactional
+	public void modify(final long memberId, final long memoId, final ModifyMemoRequest request) {
+		val memo = memoAdapter.findByIdAndMemberId(memoId, memberId);
+		memo.update(request.content());
+		memoAdapter.update(memo);
 	}
 }
