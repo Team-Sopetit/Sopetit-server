9516021b4de56a6b9dce36e16c6246ab33b1f679
Chan531 <chan657312@gmail.com>
2024-11-27 18:01:27 +0900

    feature: memo entity constructor

--- src/main/java/com/soptie/server/persistence/entity/MemoEntity.java
+++ src/main/java/com/soptie/server/persistence/entity/MemoEntity.java
@@ -2,6 +2,8 @@ package com.soptie.server.persistence.entity;
 
 import java.time.LocalDate;
 
+import com.soptie.server.domain.member.Member;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -18,4 +20,10 @@ public class MemoEntity extends BaseEntity {
 	private String content;
 	@Column(nullable = false)
 	private long memberId;
+
+	public MemoEntity(final LocalDate achievedDate, final String content, final Member member) {
+		this.achievedDate = achievedDate;
+		this.content = content;
+		this.memberId = member.getId();
+	}
 }
