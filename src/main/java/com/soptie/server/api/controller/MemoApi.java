a6c1fb29889b9bccde19f21b71670bbb5c23d3a9
Chan531 <chan657312@gmail.com>
2024-11-27 18:05:31 +0900

    feature: create memo api

--- src/main/java/com/soptie/server/api/controller/MemoApi.java
+++ src/main/java/com/soptie/server/api/controller/MemoApi.java
@@ -7,14 +7,19 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.soptie.server.api.controller.dto.request.memo.CreateMemoRequest;
 import com.soptie.server.api.controller.dto.response.SuccessResponse;
+import com.soptie.server.api.controller.dto.response.memo.CreateMemoResponse;
+import com.soptie.server.api.controller.generic.SuccessMessage;
 import com.soptie.server.domain.memo.MemoService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,8 +30,11 @@ public class MemoApi {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public SuccessResponse<?> createMemo(final Principal principal) {
-
+	public SuccessResponse<CreateMemoResponse> createMemo(final Principal principal,
+		@RequestBody final CreateMemoRequest request) {
+		val memberId = Long.parseLong(principal.getName());
+		val response = memoService.create(memberId, request);
+		return SuccessResponse.success(SuccessMessage.CREATE_MEMO.getMessage(), response);
 	}
 
 	@PatchMapping("/{memoId}")
