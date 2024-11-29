9646322dfa592092c74a148c768266a62488f0c2
Chan531 <chan657312@gmail.com>
2024-11-29 14:26:43 +0900

    feature: find by member id and createdat

--- src/main/java/com/soptie/server/persistence/repository/mission/MissionHistoryRepository.java
+++ src/main/java/com/soptie/server/persistence/repository/mission/MissionHistoryRepository.java
@@ -1,8 +1,16 @@
 package com.soptie.server.persistence.repository.mission;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.soptie.server.persistence.entity.mission.MissionHistoryEntity;
 
 public interface MissionHistoryRepository extends JpaRepository<MissionHistoryEntity, Long> {
+
+	@Query(value = "SELECT * FROM mission_history m "
+		+ "WHERE m.member_id = :memberId AND DATE(m.created_at) = :date LIMIT 1", nativeQuery = true)
+	Optional<MissionHistoryEntity> findByMemberIdAndCreatedAt(long memberId, LocalDate date);
 }
