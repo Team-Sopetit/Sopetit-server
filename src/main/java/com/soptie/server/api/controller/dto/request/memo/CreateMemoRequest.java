a6cc64779529fc4cc53f179fd746b50efec0e3f8
Chan531 <chan657312@gmail.com>
2024-11-30 18:55:47 +0900

    chore: edit dto example

--- src/main/java/com/soptie/server/api/controller/dto/request/memo/CreateMemoRequest.java
+++ src/main/java/com/soptie/server/api/controller/dto/request/memo/CreateMemoRequest.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateMemoRequest(
-	@Schema(description = "달성 날짜", example = "브라운")
+	@Schema(description = "달성 날짜", example = "2024-11-30")
 	@NotNull LocalDate achievedDate,
 	@Schema(description = "메모 내용", example = "좋은 루틴이구만.")
 	@NotNull String content
