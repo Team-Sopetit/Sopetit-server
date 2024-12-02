636d71a5c60ccfce04d6de991cdc729a0fd44f6d
Chan531 <chan657312@gmail.com>
2024-12-02 18:18:51 +0900

    feature: add routine id

--- src/main/java/com/soptie/server/persistence/entity/routine/RoutineHistoryEntity.java
+++ src/main/java/com/soptie/server/persistence/entity/routine/RoutineHistoryEntity.java
@@ -1,5 +1,6 @@
 package com.soptie.server.persistence.entity.routine;
 
+import com.soptie.server.domain.memberroutine.RoutineHistory;
 import com.soptie.server.persistence.entity.BaseEntity;
 
 import jakarta.persistence.Entity;
@@ -13,9 +14,21 @@ import lombok.NoArgsConstructor;
 public class RoutineHistoryEntity extends BaseEntity {
 	private long memberRoutineId;
 	private long memberId;
+	private long routineId;
 
-	public RoutineHistoryEntity(long memberRoutineId, long memberId) {
+	public RoutineHistoryEntity(long memberRoutineId, long memberId, long routineId) {
 		this.memberRoutineId = memberRoutineId;
 		this.memberId = memberId;
+		this.routineId = routineId;
+	}
+
+	public RoutineHistory toDomain() {
+		return RoutineHistory.builder()
+			.id(this.id)
+			.memberRoutineId(this.memberRoutineId)
+			.memberId(this.memberId)
+			.routineId(this.routineId)
+			.createdAt(this.createdAt)
+			.build();
 	}
 }
