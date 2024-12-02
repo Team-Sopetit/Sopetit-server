db0bfe371f0283245b31b27e037b40ae845591e7
Chan531 <chan657312@gmail.com>
2024-12-02 18:18:33 +0900

    feature: find memo

--- src/main/java/com/soptie/server/persistence/repository/MemoRepository.java
+++ src/main/java/com/soptie/server/persistence/repository/MemoRepository.java
@@ -1,8 +1,12 @@
 package com.soptie.server.persistence.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.soptie.server.persistence.entity.MemoEntity;
 
@@ -10,4 +14,11 @@ public interface MemoRepository extends JpaRepository<MemoEntity, Long> {
 	Optional<MemoEntity> findByIdAndMemberId(long id, long memberId);
 
 	void deleteByIdAndMemberId(long id, long memberId);
+
+	@Query("SELECT m FROM MemoEntity m WHERE m.memberId = :memberId AND m.achievedDate BETWEEN :startDate AND :endDate")
+	List<MemoEntity> findAllByMemberIdAndAchievedDateBetween(
+		@Param("memberId") long memberId,
+		@Param("startDate") LocalDate startDate,
+		@Param("endDate") LocalDate endDate
+	);
 }
