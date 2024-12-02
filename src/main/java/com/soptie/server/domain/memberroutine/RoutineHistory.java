5d073456b7488d6febe9d8ffa6693db04885af90
Chan531 <chan657312@gmail.com>
2024-12-02 18:20:54 +0900

    feature: routine history domain

---
+++ src/main/java/com/soptie/server/domain/memberroutine/RoutineHistory.java
@@ -0,0 +1,16 @@
+package com.soptie.server.domain.memberroutine;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class RoutineHistory {
+	private Long id;
+	private long memberRoutineId;
+	private long memberId;
+	private long routineId;
+	private LocalDateTime createdAt;
+}
