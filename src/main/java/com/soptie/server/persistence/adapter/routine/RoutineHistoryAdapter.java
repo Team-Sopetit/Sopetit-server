3fba8ba16fb272dbaaae11bf96fa344c27c5314a
Chan531 <chan657312@gmail.com>
2024-12-02 18:19:43 +0900

    feature: find routine history list

--- src/main/java/com/soptie/server/persistence/adapter/routine/RoutineHistoryAdapter.java
+++ src/main/java/com/soptie/server/persistence/adapter/routine/RoutineHistoryAdapter.java
@@ -1,9 +1,13 @@
 package com.soptie.server.persistence.adapter.routine;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import com.soptie.server.domain.memberroutine.MemberRoutine;
+import com.soptie.server.domain.memberroutine.RoutineHistory;
 import com.soptie.server.persistence.entity.routine.RoutineHistoryEntity;
 import com.soptie.server.persistence.repository.routine.RoutineHistoryRepository;
 
@@ -14,8 +18,9 @@ import lombok.RequiredArgsConstructor;
 public class RoutineHistoryAdapter {
 	private final RoutineHistoryRepository historyRepository;
 
-	public void save(long memberRoutineId, long memberId) {
-		historyRepository.save(new RoutineHistoryEntity(memberRoutineId, memberId));
+	public void save(final MemberRoutine memberRoutine) {
+		historyRepository.save(new RoutineHistoryEntity(memberRoutine.getId(), memberRoutine.getMemberId(),
+			memberRoutine.getRoutineId()));
 	}
 
 	public void deleteByRoutineIdAndCreatedAt(long memberRoutineId, LocalDate date) {
@@ -28,5 +33,14 @@ public class RoutineHistoryAdapter {
 
 	public boolean isExistByMemberIdAndCreatedAt(final long memberId, final LocalDate date) {
 		return historyRepository.findByMemberIdAndCreatedAt(memberId, date).isPresent();
+	}
+
+	public List<RoutineHistory> findAllByMemberIdAndCreatedAtBetween(
+		final long memberId,
+		final LocalDateTime startDateTime,
+		final LocalDateTime endDateTime
+	) {
+		return historyRepository.findAllByMemberIdAndCreatedAtBetween(memberId, startDateTime, endDateTime).stream()
+			.map(RoutineHistoryEntity::toDomain).toList();
 	}
 }
