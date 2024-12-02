7d43007a9b63b595cd98f218b12a57d2e2e5c851
Chan531 <chan657312@gmail.com>
2024-12-02 18:19:00 +0900

    feature: add mission id

--- src/main/java/com/soptie/server/persistence/entity/mission/MissionHistoryEntity.java
+++ src/main/java/com/soptie/server/persistence/entity/mission/MissionHistoryEntity.java
@@ -1,5 +1,6 @@
 package com.soptie.server.persistence.entity.mission;
 
+import com.soptie.server.domain.membermission.MissionHistory;
 import com.soptie.server.persistence.entity.BaseEntity;
 
 import jakarta.persistence.Entity;
@@ -13,9 +14,21 @@ import lombok.NoArgsConstructor;
 public class MissionHistoryEntity extends BaseEntity {
 	private long memberMissionId;
 	private long memberId;
+	private long missionId;
 
-	public MissionHistoryEntity(long memberMissionId, long memberId) {
+	public MissionHistoryEntity(long memberMissionId, long memberId, long missionId) {
 		this.memberMissionId = memberMissionId;
 		this.memberId = memberId;
+		this.missionId = missionId;
+	}
+
+	public MissionHistory toDomain() {
+		return MissionHistory.builder()
+			.id(this.id)
+			.id(this.memberMissionId)
+			.memberId(this.memberId)
+			.missionId(this.missionId)
+			.createdAt(this.createdAt)
+			.build();
 	}
 }
