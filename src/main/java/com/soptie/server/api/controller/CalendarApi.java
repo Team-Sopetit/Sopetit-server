e404ae08716b430c9dce149ff953c43ca652a491
Chan531 <chan657312@gmail.com>
2024-12-03 16:52:04 +0900

    feature: add calendar api docs

--- src/main/java/com/soptie/server/api/controller/CalendarApi.java
+++ src/main/java/com/soptie/server/api/controller/CalendarApi.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.soptie.server.api.controller.docs.CalendarApiDocs;
 import com.soptie.server.api.controller.dto.response.SuccessResponse;
 import com.soptie.server.api.controller.dto.response.calendar.GetCalendarResponse;
 import com.soptie.server.api.controller.generic.SuccessMessage;
@@ -20,7 +21,7 @@ import lombok.val;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v3/calendars")
-public class CalendarApi {
+public class CalendarApi implements CalendarApiDocs {
 
 	private final CalendarService calendarService;
 
---
+++ src/main/java/com/soptie/server/api/controller/docs/CalendarApiDocs.java
@@ -0,0 +1,54 @@
+package com.soptie.server.api.controller.docs;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.soptie.server.api.controller.dto.response.ErrorResponse;
+import com.soptie.server.api.controller.dto.response.SuccessResponse;
+import com.soptie.server.api.controller.dto.response.calendar.GetCalendarResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "CalendarApi_v3", description = "캘린더 API version3")
+public interface CalendarApiDocs {
+
+	@Operation(
+		summary = "캘린더 조회",
+		description = "루틴 달성 기록, 메모가 포함되어있는 캘린더를 조회한다.",
+		responses = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "성공",
+				content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
+			@ApiResponse(
+				responseCode = "4xx",
+				description = "클라이언트(요청) 오류",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(
+				responseCode = "500",
+				description = "서버 내부 오류",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class)))}
+	)
+	SuccessResponse<GetCalendarResponse> getCalendar(
+		@Parameter(hidden = true) Principal principal,
+		@Parameter(
+			name = "year",
+			description = "조회할 캘린더의 년도",
+			in = ParameterIn.QUERY,
+			example = "2024"
+		) @RequestParam int year,
+		@Parameter(
+			name = "month",
+			description = "조회할 캘린더의 월",
+			in = ParameterIn.QUERY,
+			example = "12"
+		) @RequestParam int month
+	);
+}
