b8703c1b12fd65fcf48910c2248bc4f0b7f289a4
Chan531 <chan657312@gmail.com>
2024-12-26 18:18:45 +0900

    fix: change table name

--- src/main/java/com/soptie/server/persistence/repository/challenge/ChallengeHistoryRepository.java
+++ src/main/java/com/soptie/server/persistence/repository/challenge/ChallengeHistoryRepository.java
@@ -13,12 +13,12 @@ import com.soptie.server.persistence.entity.challenge.ChallengeHistoryEntity;
 
 public interface ChallengeHistoryRepository extends JpaRepository<ChallengeHistoryEntity, Long> {
 
-	@Query(value = "SELECT * FROM mission_history m "
-		+ "WHERE m.member_id = :memberId AND DATE(m.created_at) = :date LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM challenge_history c "
+		+ "WHERE c.member_id = :memberId AND DATE(c.created_at) = :date LIMIT 1", nativeQuery = true)
 	Optional<ChallengeHistoryEntity> findByMemberIdAndCreatedAt(long memberId, LocalDate date);
 
-	@Query("SELECT m FROM ChallengeHistoryEntity m WHERE m.memberId = :memberId "
-		+ "AND m.createdAt BETWEEN :startDateTime AND :endDateTime")
+	@Query("SELECT c FROM ChallengeHistoryEntity c WHERE c.memberId = :memberId "
+		+ "AND c.createdAt BETWEEN :startDateTime AND :endDateTime")
 	List<ChallengeHistoryEntity> findAllByMemberIdAndCreatedAtBetween(
 		@Param("memberId") long memberId,
 		@Param("startDateTime") LocalDateTime startDateTime,
