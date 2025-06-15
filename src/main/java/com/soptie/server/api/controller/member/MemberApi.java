1df360a45ca514a78fecf408a8009f2214b2a4b3
Chan531 <chan657312@gmail.com>
2025-06-15 14:39:43 +0900

    fix: 충돌 해결

--- src/main/java/com/soptie/server/api/controller/member/MemberApi.java
+++ src/main/java/com/soptie/server/api/controller/member/MemberApi.java
@@ -13,16 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD:src/main/java/com/soptie/server/api/controller/MemberApi.java
-import com.soptie.server.api.controller.docs.MemberApiDocs;
-import com.soptie.server.api.controller.dto.request.member.CreateProfileRequest;
-import com.soptie.server.api.controller.dto.request.member.UpdateFcmTokenRequest;
-import com.soptie.server.api.controller.dto.response.SuccessResponse;
-import com.soptie.server.api.controller.dto.response.member.GetHomeInfoResponse;
-import com.soptie.server.api.controller.dto.response.member.GiveMemberCottonResponse;
-import com.soptie.server.api.controller.dto.response.member.MemberProfileResponse;
-import com.soptie.server.api.controller.generic.SuccessMessage;
-=======
 import com.soptie.server.api.common.constants.MessageConstants;
 import com.soptie.server.api.controller.generic.dto.SuccessResponse;
 import com.soptie.server.api.controller.member.docs.MemberApiDocs;
@@ -30,7 +20,7 @@ import com.soptie.server.api.controller.member.dto.CreateProfileRequest;
 import com.soptie.server.api.controller.member.dto.GetHomeInfoResponse;
 import com.soptie.server.api.controller.member.dto.GiveMemberCottonResponse;
 import com.soptie.server.api.controller.member.dto.MemberProfileResponse;
->>>>>>> develop:src/main/java/com/soptie/server/api/controller/member/MemberApi.java
+import com.soptie.server.api.controller.member.dto.UpdateFcmTokenRequest;
 import com.soptie.server.domain.member.CottonType;
 import com.soptie.server.domain.member.MemberService;
 
